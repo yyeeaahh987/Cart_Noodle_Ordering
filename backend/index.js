e0c201b9e873a533a81f210cbe58badaa1c130f6
David Chu <chu19972009@gmail.com>
2023-10-19 15:16:53 +0800

    Connect to MondoDB

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv").config;
+const dotenv = require("dotenv").config();
 const mongoose = require("mongoose");
 const app = express();
 
 //connect db
+mongoose.connect(process.env.MONGO_URL);
+
+// mongoose.connect(process.env.MONGO_URL, () =>
+//   console.log("DB is successfully connected")
+// );
 
 //start server
-
-//
+app.listen(process.env.PORT, () => {
+  console.log(`Server started on port ${process.env.PORT}`);
+});
