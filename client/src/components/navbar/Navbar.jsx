d743e7ca919246862edb4a9b7e5f5c9b0de2a6a4
David Chu <chu19972009@gmail.com>
2023-10-30 15:18:03 +0800

    - add navbar jsx and css

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,8 +1,47 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import classes from "./navbar.module.css";
+import { AiOutlineUser, AiOutlineShoppingCart } from "react-icons/ai";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className={classes.container}>
+      <div className={classes.wrapper}>
+        <div className={classes.left}>
+          <Link to="/" className={classes.title}>
+            CartNoodleOrdering
+          </Link>
+        </div>
+        <div className={classes.left}>
+          <ul className={classes.list}>
+            <li className={classes.listItem}>
+              <a href="#">Home</a>
+            </li>
+            <li className={classes.listItem}>
+              <a href="#contacts">Contacts</a>
+            </li>
+            <li className={classes.listItem}>
+              <a href="#foods">Food</a>
+            </li>
+            <li className={classes.listItem}>
+              <a href="#FAQ">FAQ</a>
+            </li>
+            <li className={classes.listItem}>
+              <a href="/create">Create</a>
+            </li>
+          </ul>
+        </div>
+        <div className={classes.right}>
+          <AiOutlineUser className={classes.userIcon} />
+          <Link to="/cart" className={classes.cartContainer}>
+            <AiOutlineShoppingCart className={classes.cartIcon} />
+            <div className={classes.cartQuantity}>0</div>
+          </Link>
+          <button className={classes.logout}>Logout</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
