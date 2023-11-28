46b03cc098d91d82fb39fbe8219ae9383819f48d
David Chu <chu19972009@gmail.com>
2023-11-28 16:00:30 +0800

    - add newsletter and footer

--- client/src/components/newsletter/Newsletter.jsx
+++ client/src/components/newsletter/Newsletter.jsx
@@ -1,8 +1,26 @@
 import React from "react";
 import classes from "./newsletter.module.css";
+import { AiOutlineSend } from "react-icons/ai";
+import newsletterIllustration from "../../assets/get-newsletter-updates.svg";
 
 const Newsletter = () => {
-  return <div>Newsletter</div>;
+  return (
+    <section id="Newsletter" className={classes.container}>
+      <div className={classes.wrapper}>
+        <h4 className={classes.subtitle}>Get our latest offers</h4>
+        <h2 className={classes.title}>Newsletter</h2>
+        <div className={classes.inputContainer}>
+          <input type="text" placeholder="Enter your email" />
+          <AiOutlineSend className={classes.sendIcon} />
+        </div>
+        <img
+          src={newsletterIllustration}
+          className={classes.illustration}
+          alt=""
+        ></img>
+      </div>
+    </section>
+  );
 };
 
 export default Newsletter;
