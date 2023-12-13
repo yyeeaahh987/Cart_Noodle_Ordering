2aa6142b11d67c825d90d8a8c7027c91f7481e4c
David Chu <chu19972009@gmail.com>
2023-12-13 21:46:34 +0800

    - ca cert

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,18 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv").config();
 const mongoose = require("mongoose");
+const https = require("https");
+const fs = require("fs");
+// import fs from 'fs';
 const authController = require("./controllers/authController");
 const productController = require("./controllers/productController");
 const cashController = require("./controllers/cashController");
 const app = express();
 
 //connect db
-mongoose.connect(process.env.MONGO_URL).then(() => {console.log("Database Connected");})
+mongoose.connect(process.env.MONGO_URL).then(() => {
+  console.log("Database Connected");
+});
 
 //routes & middlewares
 app.use(cors());
@@ -19,4 +24,20 @@ app.use("/product", productController);
 app.use("/cash", cashController);
 
 //start server
-app.listen(process.env.PORT, () => {console.log(`Server started on port ${process.env.PORT}`);});
+app.listen(process.env.PORT, () => {
+  console.log(`Server started on port ${process.env.PORT}`);
+});
+
+const key = fs.readFileSync(`${__dirname}/private.key`);
+const cert = fs.readFileSync(`${__dirname}/certificate.crt`);
+
+const cred = {
+  key,
+  cert,
+};
+
+const httpsPort = 8443;
+
+const httpsServer = https.createServer(cred, app);
+httpsServer.listen(httpsPort);
+console.log(`https App is listening on port ${httpsPort} !`);
